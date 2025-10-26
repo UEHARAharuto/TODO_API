d6c1ffdde0accd3c60ce93e2662bcfe4cbd48016
uehara <tottokun27@gmail.com>
2025-10-26 23:00:07 +0900

    marge

--- handler/todo_handler.go
+++ handler/todo_handler.go
@@ -125,7 +125,7 @@ func UpdateTodo(c *gin.Context) {
 	result, err := db.DB.Exec(sql, req.Title, req.Status, priority, now, id)
 	if err != nil {
 		log.Printf("ERROR: Failed to update todo: %v", err)
-		c.Status(http.StatusNotImplemented)
+		c.Status(http.StatusInternalServerError)
 		return
 	}
 
