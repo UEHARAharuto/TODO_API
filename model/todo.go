c2589035c512050c12c095b453d69058089dea10
uehara <tottokun27@gmail.com>
2025-10-26 17:51:45 +0900

    db/db.go の CREATE TABLE 文に priority カラムを追加 (デフォルト値: 100)。 model/todo.go の Todo 構造体に Priority フィールドを追加。 CreateTodo, UpdateTodo ハンドラを修正し、priority を保存できるようにした。(0の場合はデフォルト値100をセット) GetTodos` (一覧取得) ハンドラを修正し、priority ASC(昇順) でソートされた結果を返すようにSQLを変更した。

--- model/todo.go
+++ model/todo.go
@@ -5,20 +5,7 @@ import "time"
 type Todo struct {
 	ID        uint      `json:"id"`
 	Title     string    `json:"title"`
+	Priority  int       `json:"priority"`
 	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
-}
-
-type CreateTodoRequest struct {
-	Title string `json:"title"`
-}
-
-type UpdateTodoRequest struct {
-	Title string `json:"title"`
-}
-
-type TodoResponse struct {
-	ID        uint      `json:"id"`
-	Title     string    `json:"title"`
 	UpdatedAt time.Time `json:"updatedAt"`
 }
